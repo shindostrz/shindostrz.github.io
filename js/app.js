0cc7c3c2f928154fa073ee9997e7820e9cc89d32
Shindo Strzelczyk <shindostrz@gmail.com>
2014-05-16 17:04:16 -0700

    Added jQuery logic to make the menu button show on resize and display the links on click instead of hover

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
+  if ($(window).width() <= "640") {
+  $('#section-links').removeClass("section-links");
+  $("#section-links").addClass("menu-button");
+}
   $('a[href*=#]').click(function() {
+    if ($(window).width() <= "640") {
+    $("#section-links").toggleClass("menu-display");
+    $("#section-links").toggleClass("menu-button");
+  }
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
     && location.hostname == this.hostname) {
       var $target = $(this.hash);
@@ -13,10 +21,23 @@ $(document).ready(function(){
       }
     }
   });
+
   $(window).resize(function() {
     if ($(".image img").css("width") == $(window).width().toString() + "px") {
       $(".description").height($(".image img").height() - 10);
       console.log("resized");
     }
+  if ($(window).width() <= "640") {
+    $('#section-links').removeClass("section-links");
+    $("#section-links").addClass("menu-button");
+  }
+  if ($(window).width() > "640") {
+    $("#section-links").removeClass("menu-button");
+    $('#section-links').addClass("section-links");
+  }
+  });
+  $("ul").click(function() {
+    $("#section-links").toggleClass("menu-display");
+    $("#section-links").toggleClass("menu-button");
   });
 });
